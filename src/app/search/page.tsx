c655c0b6ff57f80e1734b25a8ce87803d2dcbbb5
sempitrnal <reynaldsampelo@gmail.com>
2024-12-17 18:01:57 +0800

    fix

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -11,7 +11,6 @@ import {
 } from "@/components/ui/breadcrumb";
 import { urlFor } from "@/sanity/client";
 import { AnimatePresence, motion } from "framer-motion";
-import Image from "next/image";
 import Link from "next/link";
 import { useSearchParams } from "next/navigation";
 import { Suspense, useEffect, useState } from "react";
